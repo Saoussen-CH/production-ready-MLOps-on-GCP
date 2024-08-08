a5e7d4dc1ba538d5da1c52b465b5dd3ba832e995
Saoussen-CH <saoussen.chaabnia.pro@gmail.com>
2024-08-08 16:15:58 +0100

    feat: add a service account to run vertex ai pipelines and parametrize enbale_caching in the kubeflow pipeline

--- pipelines/src/pipelines/utils/trigger_pipeline.py
+++ pipelines/src/pipelines/utils/trigger_pipeline.py
@@ -7,20 +7,24 @@ def trigger_pipeline(
     template_path: str,
     display_name: str,
     type: str,
+    enable_caching: bool = False,
 ) -> aiplatform.pipeline_jobs.PipelineJob:
     """Trigger a Vertex Pipeline run from a (local) compiled pipeline definition.
 
     Args:
         template_path (str): file path to the compiled YAML pipeline
         display_name (str): Display name to use for the PipelineJob
+        type (str): Type of the pipeline to trigger <training/prediction>
+        enable_caching (bool): Whether to enable caching for the pipeline
 
     Returns:
-        aiplatform.pipeline_jobs.PipelineJob: the Vertex PipeliclearneJob object
+        aiplatform.pipeline_jobs.PipelineJob: the Vertex PipelineJob object
     """
 
     project = env.get("VERTEX_PROJECT_ID")
     location = env.get("VERTEX_LOCATION")
     bucket_uri = env.get("VERTEX_PIPELINE_ROOT")
+    service_account = env.get("VERTEX_SA_EMAIL")
 
     if type == "training":
         parameters = {
@@ -40,11 +44,11 @@ def trigger_pipeline(
         template_path=template_path,
         parameter_values=parameters,
         pipeline_root=bucket_uri,
-        enable_caching=True,
+        enable_caching=enable_caching,
         location=location,
     )
 
-    start_pipeline.run()
+    start_pipeline.run(service_account=service_account)
 
     start_pipeline.wait()
 
@@ -66,8 +70,15 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "--type",
-        help="Type of the pipeline to tirgger <training/prediction>",
+        help="Type of the pipeline to trigger <training/prediction>",
         type=str,
+    )
+
+    parser.add_argument(
+        "--enable_caching",
+        help="Whether to enable caching for the pipeline",
+        type=bool,
+        default=False,
     )
 
     args = parser.parse_args()
@@ -76,4 +87,5 @@ if __name__ == "__main__":
         template_path=args.template_path,
         display_name=args.display_name,
         type=args.type,
+        enable_caching=args.enable_caching,
     )
