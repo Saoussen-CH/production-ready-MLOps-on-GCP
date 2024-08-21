1210a9988c863597ac13ab83beac27d91886f4e6
Saoussen-CH <saoussen.chaabnia.pro@gmail.com>
2024-08-21 18:47:28 +0100

    refactor: separate the scheduling of the training and prediction pipelines into different steps and remove wait for the scheduling pipeline execution

--- pipelines/src/pipelines/utils/schedule_pipeline.py
+++ pipelines/src/pipelines/utils/schedule_pipeline.py
@@ -1,6 +1,7 @@
 import argparse
 from google.cloud import aiplatform
 from os import environ as env
+import logging
 
 
 def schedule_pipeline(
@@ -58,8 +59,7 @@ def schedule_pipeline(
         display_name=schedule_name,
         service_account=service_account,
     )
-    pipeline_job_schedule.wait()
-    print(f"Schedule created: {pipeline_job_schedule}")
+    logging.info(f"Schedule created: {pipeline_job_schedule}")
 
     return pipeline_job_schedule
 
